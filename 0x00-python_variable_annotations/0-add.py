10e3c23aa2e0c2a874c37be339d209e0a2f328de
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-11 14:12:33 +0100

    fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,10 @@
+#!/usr/bin/env python3
+"""
+0. Basic annotations - add
+"""
 def add(a: float, b: float) -> float:
+    """
+    takes a float a and a float b as arguments
+    and returns their sum as a float
+    """
     return a + b
