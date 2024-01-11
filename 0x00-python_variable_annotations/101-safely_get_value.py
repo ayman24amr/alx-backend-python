79b2a5e2432d30e68e9b41ddada1b76f39cf0683
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-11 17:20:03 +0100

    12

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,7 @@ def safely_get_value(
                      dct: Mapping,
                      key: Any,
                      default: Union[T, None] = None
-    ) -> Union[Any, T]:
+                    ) -> Union[Any, T]:
     """
     function
     """
