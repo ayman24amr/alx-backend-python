88e4ef3f3fc889e6a59314673f8883cd92a13168
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-15 20:12:18 +0100

    0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+0. The basics of async
+"""
+import random, time
+
+
+async def wait_random(max_delay = 10):
+    """
+    waits for a random delay between 0 and max_delay seconds
+    and eventually returns it
+    """
+    ran =  random.random() * max_delay
+    time.sleep(ran)
+    return ran
